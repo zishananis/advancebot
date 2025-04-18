9ce5b62075d0a4c57dafa418e980acad1ca8f1c4
HiiFyi <167199917+HiiFyi@users.noreply.github.com>
2025-04-18 10:56:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,8 +13,8 @@ def is_enabled(value, default):
 #---------------------------------------------------------------
 #---------------------------------------------------------------         ,
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
+API_ID = int(environ.get('API_ID', '25251875'))
+API_HASH = environ.get('API_HASH', '9f413b540c859573a91299d252e6e389')
 BOT_TOKEN = environ.get('BOT_TOKEN', '')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
