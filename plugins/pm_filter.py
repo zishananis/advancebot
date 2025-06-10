c14040206e793c44e75f2ee4f5b5e3cb69833d52
HiiFyi <167199917+HiiFyi@users.noreply.github.com>
2025-06-10 07:17:23 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1440,7 +1440,7 @@ async def auto_filter(client, msg, spoll=False , pm_mode = False):
         if not files:
             await client.send_message(req_channel,f"🦋 **#REQUESTED_CONTENT** 🦋,\n\n📝**CONTENT NAME** : `{search}`\n**REQUESTED BY** : {message.from_user.first_name}\n **USER ID : **{message.from_user.id}\n\n🗃️",reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🔺 Mark as Done 🔺", callback_data="close_data")]]))
             g = await message.reply_text(text=f"""Your File Not Found in Database 🚫""")
-            await asyncio.sleep(60)
+            await asyncio.sleep(6)
             await g.delete()
             
 	    
