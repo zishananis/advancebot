1bd20a25c9953a16711361c55a881091dd037a10
HiiFyi <167199917+HiiFyi@users.noreply.github.com>
2025-06-18 10:00:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -19,9 +19,9 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6942557751 5446981726').split()]
-USERNAME = environ.get('USERNAME', "") # ADMIN USERNAME WITH https://
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
-MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', '') # WITH https://
+USERNAME = environ.get('USERNAME', "https://t.me/zishananis_official") # ADMIN USERNAME WITH https://
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002340773096'))
+MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/filmsexpert2') # WITH https://
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002445219066').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------#kdj
